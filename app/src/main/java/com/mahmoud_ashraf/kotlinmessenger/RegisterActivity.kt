3d21af44de9472dcdbf2b7751bc6af808ea82ba8
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2019-03-01 20:15:41 +0200

    add select_img feature

--- app/src/main/java/com/mahmoud_ashraf/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/mahmoud_ashraf/kotlinmessenger/RegisterActivity.kt
@@ -1,18 +1,26 @@
 package com.mahmoud_ashraf.kotlinmessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class MainActivity : AppCompatActivity() {
-
+class RegisterActivity : AppCompatActivity() {
+    /**By declaring a companion object inside our class,
+    you’ll be able to call its members with the same syntax as calling static methods in Java/C#,
+    using only the class name as a qualifier*/
+    companion object {
+        val TAG = "RegisterActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
 
         // you can access view in kotlin by its id only
@@ -24,16 +32,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         already_have_account_text_view.setOnClickListener {
-            Log.d("MainActivity", "Try to show login activity")
+            Log.d("RegisterActivity", "Try to show login activity")
 
             // launch the login activity somehow
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
+        selectphoto_button_register.setOnClickListener {
+            Log.d(TAG, "Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+
+
 
     }
+    var selectedPhotoUri: Uri? = null
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // proceed and check what the selected image was....
+            Log.d(TAG, "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            selectphoto_imageview_register.setImageBitmap(bitmap)
+
+            // make the button transparent ;)
+            selectphoto_button_register.alpha = 0f
+
+
+        }
+    }
     private fun performRegister(){
         val email = email_edittext_register.text.toString()
         val password = password_edittext_register.text.toString()
@@ -43,8 +81,8 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        Log.d("MainActivity", "Email is: " + email)
-        Log.d("MainActivity", "Password: $password")
+        Log.d("RegisterActivity", "Email is: " + email)
+        Log.d("RegisterActivity", "Password: $password")
 
         // Firebase Authentication to create a user with email and password
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
