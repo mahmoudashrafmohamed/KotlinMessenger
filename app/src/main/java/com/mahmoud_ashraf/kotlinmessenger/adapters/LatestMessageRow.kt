655ba85cee801213a8d8f31ae2d00206af3498a7
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2019-03-05 16:22:53 +0200

    added username and profile image to latest messages

--- app/src/main/java/com/mahmoud_ashraf/kotlinmessenger/adapters/LatestMessageRow.kt
+++ app/src/main/java/com/mahmoud_ashraf/kotlinmessenger/adapters/LatestMessageRow.kt
@@ -1,7 +1,14 @@
 package com.mahmoud_ashraf.kotlinmessenger.adapters
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.letsbuildthatapp.kotlinmessenger.models.ChatMessage
 import com.mahmoud_ashraf.kotlinmessenger.R
+import com.mahmoud_ashraf.kotlinmessenger.models.User
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.latest_message_row.view.*
@@ -11,7 +18,29 @@ import kotlinx.android.synthetic.main.latest_message_row.view.*
  */
 class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.message_textview_latest_message.text = chatMessage.text
+        val chatPartnerId: String
+
+        if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+            chatPartnerId = chatMessage.toId
+        } else {
+            chatPartnerId = chatMessage.fromId
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val user = p0.getValue(User::class.java)
+                viewHolder.itemView.username_textview_latest_message.text = user?.username
+
+                Picasso.get().load(user?.profileImageUrl).into(viewHolder.itemView.imageview_latest_message)
+            }
+
+        })
+       viewHolder.itemView.message_textview_latest_message.text = chatMessage.text
     }
 
     override fun getLayout(): Int {
